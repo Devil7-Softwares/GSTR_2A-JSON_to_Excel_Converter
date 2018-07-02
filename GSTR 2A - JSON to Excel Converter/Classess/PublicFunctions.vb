5e72d85251cf2d07d090fdb41d86f887606fb462
Devil7DK <dineshthangavel47@gmail.com>
2018-07-02 17:08:10 +0530

    Functions: Implement 'isJsonZIP' function to make sure the given zip contains json

---
+++ GSTR 2A - JSON to Excel Converter/Classess/PublicFunctions.vb
@@ -0,0 +1,18 @@
+﻿Imports Ionic.Zip
+
+Module PublicFunctions
+
+    Function isJsonZIP(ByVal Path As String) As Boolean
+        Using zip = ZipFile.Read(Path)
+            Dim totalEntries As Integer = zip.Entries.Count
+            For Each e As ZipEntry In zip.Entries
+                If e.FileName.ToLower.EndsWith(".json") Then
+                    Return True
+                    Exit For
+                End If
+            Next
+        End Using
+        Return False
+    End Function
+
+End Module
