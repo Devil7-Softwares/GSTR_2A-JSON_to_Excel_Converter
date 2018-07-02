3bff7319dc4d01b655ed574aee4cd4d71264a8bb
Devil7DK <dineshthangavel47@gmail.com>
2018-07-02 17:03:10 +0530

    Create Custom Object to Hold File Details

---
+++ GSTR 2A - JSON to Excel Converter/Classess/JSON/JSONFile.vb
@@ -0,0 +1,22 @@
+﻿Public Class JSONFile
+    Dim filepath As String = ""
+    Sub New(ByVal FilePath As String)
+        Me.filepath = FilePath
+    End Sub
+    ReadOnly Property Filename As String
+        Get
+            Return IO.Path.GetFileName(filepath)
+        End Get
+    End Property
+    ReadOnly Property Path As String
+        Get
+            Return filepath
+        End Get
+    End Property
+    Public Overrides Function ToString() As String
+        Return Filename
+    End Function
+    Function isZip() As Boolean
+        Return filepath.ToLower.EndsWith(".zip")
+    End Function
+End Class
